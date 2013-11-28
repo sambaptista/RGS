1c2ce5b9e8967e4a51fd85ca5de4b349881a0d95
Sam Baptista <samuel.baptista@ecodev.ch>
2013-11-28 18:54:24 +0100

    Secure and ignore settings files

---
+++ wpimporter/config/settings-sample.php
@@ -0,0 +1,23 @@
+<?php 
+
+/**
+ *	CONNEXION AU SERVEUR DE LA BD
+ *
+ *  Rename this file settings.php and set logins
+*/
+define('HOST', 'outstanding host name');
+define('LOGIN', "outstanding user name");
+define('PASSWORD', "outstanding protected password");
+
+// Nom BD de typo
+define('BD_NAME' , "outstanding typo database name");
+
+// Nom BD de Wordpress
+define('BD_NAME_WP' , "outstanding wordpress database name");
+
+// executable mysqldump
+define('MYSQLDUMP', '/usr/local/bin/mysqldump');
+define('MYSQL', '/usr/local/bin/mysql');
+
+
+?>
